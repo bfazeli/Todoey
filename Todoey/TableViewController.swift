a3dff30d36ad001496179b3f1b33af49f1ceee88
bijan102 <bfazeli1@gmail.com>
2018-06-07 06:12:59 -0700

    Initial commit: Set up TableViewController and a nav controller w/ dummy data.

---
+++ Todoey/TableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Bijan Fazeli on 6/5/18.
+//  Copyright © 2018 Fazeli, Bijan. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    let sports: [String] = ["Soccer", "Basketball", "Tennis", "Volleyball"]
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sports.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = "I play " + sports[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+
+}
+
